0c219e416ef6d36180b3f4abdb0b5eb769da7512
1 <a1@1demacbook-air.local>
2016-01-06 21:47:35 +0800

    Initial Commit

---
+++ 06-轮播图片/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  06-轮播图片
+//
+//  Created by 1 on 16/1/6.
+//  Copyright © 2016年 1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 06-轮播图片/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  06-轮播图片
+//
+//  Created by 1 on 16/1/6.
+//  Copyright © 2016年 1. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
